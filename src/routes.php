94f40fa054448560dd073e09dbef948676d9e2d2
arata <aratamagna@gmail.com>
2018-07-08 20:17:04 -0400

    landing page

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,8 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $this->logger->info("");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.html', $args);
 });
