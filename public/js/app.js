01a720c2ab23ade3f6c72bab1cd28258f64ed63b
arata <aratamagna@gmail.com>
2018-07-18 16:23:56 -0400

    log prop

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ $(function() {
 
 function init(id){
   $.get( "doc/"+id, function( data ) {
-    console.log(data)
+    console.log(data.name)
     $("#doc_title").text( ""+data.name );
   }, "json");
 }
