201bd38c0d61ad277c944b87be8c810de7dabdf3
arata <aratamagna@gmail.com>
2018-07-18 19:06:56 -0400

    clear fields

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ $(function() {
     $.get( "doc/"+id, function( data ) {
       form = jQuery.parseJSON(data);
       $("#doc_title").text( form.name );
-      $(".field_container").remove();
+      $(".field_container").empty();
       $.each(form.fields, function( index, value ) {
         var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
         $(".field_container").append($(field));
