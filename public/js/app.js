d5a32e461a96c9924665e32bc3ad6999343f360a
arata <aratamagna@gmail.com>
2018-07-18 19:08:03 -0400

    clear fileds fix

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,6 @@ $(function() {
     $.get( "doc/"+id, function( data ) {
       form = jQuery.parseJSON(data);
       $("#doc_title").text( form.name );
-      $(".field_container").empty();
       $.each(form.fields, function( index, value ) {
         var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
         $(".field_container").append($(field));
@@ -36,6 +35,7 @@ $(function() {
   }
 
   $("#smartwizard").on("showStep", function(e, anchorObject, stepNumber, stepDirection) {
+    $(".field_container").empty();
     if(stepNumber == 1){
       init(doc);
     }
