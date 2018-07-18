5699ecfaf63036d7fc2de5d0e438d651a614c8d3
arata <aratamagna@gmail.com>
2018-07-18 16:29:42 -0400

    show name

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ $(function() {
 
 function init(id){
   $.get( "doc/"+id, function( data ) {
-    console.log(data['name'])
-    $("#doc_title").text( ""+data.name );
+    $("#doc_title").text( data.name );
   }, "json");
 }
