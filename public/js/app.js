ac684d9f18abacd18a0364c811aa88b35584e990
arata <aratamagna@gmail.com>
2018-07-18 16:40:42 -0400

    app v3

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,14 @@
 $(function() {
-  init(2)
+  init(3)
 });
 
 function init(id){
   $.get( "doc/"+id, function( data ) {
     form = jQuery.parseJSON(data);
     $("#doc_title").text( form.name );
+    $.each(form.fields, function( index, value ) {
+      var field = '<div class="form-group"><label>'+value.label+'</label>'+value.tag+'<small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
+      console.log(field)
+    });
   }, "json");
 }
