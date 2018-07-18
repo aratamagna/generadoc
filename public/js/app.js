d46f0e17ba0d9f032b3c343b4d9a9ff9a8ec738f
arata <aratamagna@gmail.com>
2018-07-18 17:40:57 -0400

    steps

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(function() {
-  init(1)
+  init()
 });
 
 function init(id){
@@ -11,4 +11,40 @@ function init(id){
       $(".field_container").append($(field));
     });
   }, "json");
+}
+
+$(document).ready(function () {
+    //Initialize tooltips
+    $('.nav-tabs > li a[title]').tooltip();
+
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
 }
