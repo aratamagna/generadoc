4114f22609f9200b80fc5eb3645d1b37fdc2cd9e
arata <aratamagna@gmail.com>
2018-07-18 18:24:06 -0400

    tunning

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,29 @@
 $(function() {
-  init()
-});
+  init(1);
+  $('#smartwizard').smartWizard({
+    selected: 0,
+    autoAdjustHeight:true,
+    cycleSteps: false,
+    lang: {  // Language variables
+      next: 'Siguiente',
+      previous: 'Volver'
+    },
+    toolbarSettings: {
+      toolbarPosition: 'bottom',
+      toolbarButtonPosition: 'right',
+      showNextButton: true,
+      showPreviousButton: true
+    },
+    theme: 'dots');
+  });
 
-function init(id){
-  $.get( "doc/"+id, function( data ) {
-    form = jQuery.parseJSON(data);
-    $("#doc_title").text( form.name );
-    $.each(form.fields, function( index, value ) {
-      var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
-      $(".field_container").append($(field));
-    });
-  }, "json");
-}
+  function init(id){
+    $.get( "doc/"+id, function( data ) {
+      form = jQuery.parseJSON(data);
+      $("#doc_title").text( form.name );
+      $.each(form.fields, function( index, value ) {
+        var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
+        $(".field_container").append($(field));
+      });
+    }, "json");
+  }
