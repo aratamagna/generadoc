19bd36aec0005c8823422b1de3f692b2c1089c71
arata <aratamagna@gmail.com>
2018-07-18 17:14:14 -0400

    app v6

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,7 @@ function init(id){
     $("#doc_title").text( form.name );
     $.each(form.fields, function( index, value ) {
       var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
-      console.log(field)
-      $(".field_container").append(field);
+      $(".field_container").append($(field));
     });
   }, "json");
 }
