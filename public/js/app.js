dd9ed5420698d98047c28852a9bdb7c4a6af8f48
arata <aratamagna@gmail.com>
2018-08-15 12:35:43 -0300

    list contrato plazo fijo

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,26 @@ $(function() {
   init(1);
   initapp();
   window.scrollTo(0, 0);
-  });
+});
 
-  var doc = 1;
+var doc = 1;
 
-  function init(id){
-    $.get( "doc/"+id, function( data ) {
-      form = jQuery.parseJSON(data);
-      $("#doc_title").text( form.name );
-      $.each(form.fields, function( index, value ) {
-        var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
-        $(".field_container").append($(field));
-      });
-    }, "json");
-  }
+function init(id){
+  $.get( "doc/"+id, function( data ) {
+    form = jQuery.parseJSON(data);
+    $("#doc_title").text( form.name );
+    $.each(form.fields, function( index, value ) {
+      var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
+      $(".field_container").append($(field));
+    });
+  }, "json");
+}
 
-  function initapp() {
-    var wizard = $("#wizard").steps({
+function initapp() {
+  var wizard = $("#wizard").steps({
     headerTag: "h1",
     bodyTag: "section",
     transitionEffect: "slideLeft",
     autoFocus: true
-});
-  }
+  });
+}
