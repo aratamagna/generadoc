7016ae15b65eb41a11e771c2ac558eb9f702c997
arata <aratamagna@gmail.com>
2018-08-16 17:51:12 -0300

    form container

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,13 @@ var doc = 0;
 function init(id){
   $.get( "doc/"+id, function( data ) {
     form = jQuery.parseJSON(data);
+    var container = "";
     $("#doc_title").text( form.name );
     $.each(form.fields, function( index, value ) {
-      var field = '<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
-      $(".field_container").append($(field));
+      var container = container+'<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
     });
   }, "json");
+  $(".field_container").append($(container));
 }
 
 function initapp() {
