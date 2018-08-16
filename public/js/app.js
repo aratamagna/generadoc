aeafb75a3a89d4a45b9bd002ecb4b04de4d18541
arata <aratamagna@gmail.com>
2018-08-16 17:53:53 -0300

    fix container

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,9 @@ $(function() {
 var doc = 0;
 
 function init(id){
+  var container = "";
   $.get( "doc/"+id, function( data ) {
     form = jQuery.parseJSON(data);
-    var container = "";
     $("#doc_title").text( form.name );
     $.each(form.fields, function( index, value ) {
       var container = container+'<div class="form-group"><label>'+value.label+'</label><'+value.tag+' class="form-control" type="'+value.type+'" name="'+value.label+'"><small id="HelpBlock" class="form-text text-muted">'+value.help+'</small></div>';
